d6bc4559df56a97ce20d4eadbfba0b8d1e52ae5c
Kritarth Patel <kritarthpatel2303@gmail.com>
2025-08-13 15:13:22 +0530

    Add files via upload

---
+++ Kritarth_25BCHT069.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+
+    int a,b,c;
+    system("cls");
+    //clrscr();
+    printf("Enter 2 Nos.");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d + %d = %d\n",a,b,c);
+    getch();
+
+    }
